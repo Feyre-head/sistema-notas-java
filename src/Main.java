bf145b9afd9601f24c296a8828d26d23dfab2972
Feyre <inaraoenning@gmail.com>
2025-09-15 21:15:59 -0300

    Add Acessar nota / Remover nota

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,5 @@
 import java.util.Scanner;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
@@ -14,34 +12,55 @@ public class Main {
         double[][] notasAlunos = new double[3][3];
         String[] nomesAlunos = new String[3];
 
-        boolean menu = true;
 
-        int opcao = 0;
+        int opcao = 9;
 
-        while (menu) {
+        while (opcao > 0) {
             System.out.println("""
                     1 - Adicionar Notas
                     2 - Remover Nota
                     3 - Calcular Média de um Aluno
                     4 - Exibir todas as notas
-                    0 - Sair 
+                    0 - Sair
                     :
                     """);
             opcao = sc.nextInt();
+            sc.nextLine();
 
             //Add notas
             if (opcao == 1) {
                 for (int c = 0; c < 3; c++) {
-                    sc.nextLine();
-                    System.out.println("Informe o nome do Aluno: ");
+                    System.out.print("Informe o nome do Aluno: ");
                     nomesAlunos[c] = sc.nextLine();
+
                     for (int n = 0; n < 3; n++) {
-                        System.out.println("Digite a " + (n + 1) + " nota :");
+                        System.out.print("Informe a: " + (n + 1) + " nota:");
                         notasAlunos[c][n] = sc.nextDouble();
                     }
+                    sc.nextLine();
+
+                }
+            } else if (opcao == 2) {
+
+                System.out.println("Alunos Disponiveis:");
+                int indiceAluno = 0;
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Índice: " + (i) + " Nome: " + nomesAlunos[i]);
+                }
+                System.out.println("Digite o Índice do Aluno que deseja remover a nota: ");
+                indiceAluno = sc.nextInt();
+
+                //Remover
+                for (int r = 0; r < 3; r++) {
+                    notasAlunos[indiceAluno][r] = 0.0;
+                }
+
+                //Acessar nota
+                for (int i = 0; i < 3; i++) {
+                    System.out.println("Nome: " + nomesAlunos[i] + "Nota " + (i + 1) + ": " + notasAlunos[indiceAluno][i]);
                 }
             }
         }
-
     }
+
 }
